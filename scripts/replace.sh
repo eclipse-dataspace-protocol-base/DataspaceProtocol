2a7d089cbe794c1f669ff1d267bdd5cdd90979e7
arnoweiss <arno.weiss@sap.com>
2024-10-02 20:19:42 +0200

    feat: add static respec rendering

---
+++ scripts/replace.sh
@@ -0,0 +1,25 @@
+cp scripts/respec-template.html index.html
+mkdir ./message
+mkdir ./message/diagram
+cp ./**/message/diagram/*.png ./message/diagram/
+index=`cat index.html`
+ovph="THIS IS THE PLACEHOLDER FOR THE OVERVIEW"
+modph="THIS IS THE PLACEHOLDER FOR THE MODEL"
+comph="THIS IS THE PLACEHOLDER FOR THE COMMON FUNCTIONALITIES"
+cataph="THIS IS THE PLACEHOLDER FOR THE CATALOG PROTOCOL"
+cnph="THIS IS THE PLACEHOLDER FOR THE CONTRACT NEGOTIATION PROTOCOL"
+tpph="THIS IS THE PLACEHOLDER FOR THE TRANSFER PROCESS PROTOCOL"
+ov=`cat README.md`
+mod="`cat model/terminology.md`"$'\n\n'"`cat model/model.md`"
+cata="`cat catalog/catalog.protocol.md`"$'\n\n'"`cat catalog/catalog.binding.https.md`"
+com="`cat common/common.protocol.md`"$'\n\n'"`cat common/common.binding.https.md`"
+cata="`cat catalog/catalog.protocol.md`"$'\n\n'"`cat catalog/catalog.binding.https.md`"
+cn="`cat negotiation/contract.negotiation.protocol.md`"$'\n\n'"`cat negotiation/contract.negotiation.binding.https.md`"
+tp="`cat transfer/transfer.process.protocol.md`"$'\n\n'"`cat transfer/transfer.process.binding.https.md`"
+index="${index/$ovph/"$ov"}"
+index="${index/$modph/"$mod"}"
+index="${index/$comph/"$com"}"
+index="${index/$cataph/"$cata"}"
+index="${index/$cnph/"$cn"}"
+index="${index/$tpph/"$tp"}"
+echo "$index" > index.html -e
