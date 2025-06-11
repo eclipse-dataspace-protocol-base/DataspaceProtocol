9330d9e8fdc0144c0e89e613bd8d80a0f2f21c53
Jim Marino <jim.marino@gmail.com>
2025-06-11 21:25:33 +0200

    Make endpoint property optional

--- artifacts/src/test/java/org/eclipse/dsp/schema/transfer/TransferRequestMessageSchemaTest.java
+++ artifacts/src/test/java/org/eclipse/dsp/schema/transfer/TransferRequestMessageSchemaTest.java
@@ -34,6 +34,7 @@ public class TransferRequestMessageSchemaTest extends AbstractSchemaTest {
     @Test
     void verifyMinimalRequestSchema() {
         assertThat(schema.validate(MINIMAL_REQUEST, JSON)).isEmpty();
+        assertThat(schema.validate(NO_DA_ENDPOINT, JSON)).isEmpty();
     }
 
     @BeforeEach
@@ -50,6 +51,23 @@ public class TransferRequestMessageSchemaTest extends AbstractSchemaTest {
               "consumerPid": "urn:uuid:32541fe6-c580-409e-85a8-8a9a32fbe833",
               "agreementId": "urn:uuid:e8dc8655-44c2-46ef-b701-4cffdc2faa44",
               "format": "example:HTTP_PUSH",
+              "callbackAddress": "https://example.com/callback"
+            }
+            """;
+
+    private static final String NO_DA_ENDPOINT = """
+            {
+              "@context": [
+                "https://w3id.org/dspace/2025/1/context.jsonld"
+              ],
+              "@type": "TransferRequestMessage",
+              "consumerPid": "urn:uuid:32541fe6-c580-409e-85a8-8a9a32fbe833",
+              "agreementId": "urn:uuid:e8dc8655-44c2-46ef-b701-4cffdc2faa44",
+              "format": "example:HTTP_PUSH",
+              "dataAddress": {
+                "@type": "DataAddress",
+                "endpointType": "https://w3id.org/idsa/v4.1/HTTP"
+              },
               "callbackAddress": "https://example.com/callback"
             }
             """;
